342d38b8638ab64c3ad20d40c268e4cf34ed5c41
why20w18 <enco3162@gmail.com>
2024-10-24 15:40:22 +0300

    main segment

---
+++ src/packSourceCode/Main.java
@@ -0,0 +1,14 @@
+package packSourceCode;
+import packGuiSwing.homeFrame;
+
+public class Main {
+    
+    public static void main(String[] args){
+        System.out.println("Main");
+        
+        homeFrame home_frame = new homeFrame("KUTUPHANE OTOMASYONU BASLANGIC",750,450);
+        home_frame.startFrame();
+        
+    }
+    
+}
