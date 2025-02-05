553fa544aa0c37c5185d2f6c4f676b53633563db
bugra <147606593+why20w18@users.noreply.github.com>
2025-02-06 02:29:08 +0300

    Update Main.java

--- src/packSourceCode/Main.java
+++ src/packSourceCode/Main.java
@@ -5,13 +5,24 @@ import packGuiSwing.mainGUI;
 import packDatabase.databaseBaglanti;
 import packDatabase.DatabaseIslemler;
 
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args){        
-        System.out.println("Main");
+        System.out.print("Veritabani adini girin : ");
+        Scanner cin = new Scanner(System.in);
+        String dbName = cin.nextLine();
+
+        System.out.print("Veritabani parolanizi girin : ");
+        String dbPass = cin.nextLine();
+
+        System.out.print("Veritabani port numarasini girin : ");
+        int portNo = cin.nextInt();
+
         pencereGUI.setDebugOUTPUT(false);
         //VERITABANI BAGLANTISI
-        databaseBaglanti database = new databaseBaglanti("qw","kutuphaneOtomasyon",3306);
+        databaseBaglanti database = new databaseBaglanti(dbPass,dbName,portNo);
         database.databaseBaglan();
         
         DatabaseIslemler dbIslemler = new DatabaseIslemler(database);
